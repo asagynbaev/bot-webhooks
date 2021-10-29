254c3a5ad9bd4ceb2ea60af006610810adf90509
Azimbek Sagynbaev <sagynbaev6@gmail.com>
2021-10-29 16:50:20 +0200

    First trades

--- Controllers/WebHookReceiverController.cs
+++ Controllers/WebHookReceiverController.cs
@@ -32,26 +32,58 @@ namespace bot_webhooks.Controllers
         [HttpPost]
         public void Post([FromBody]Position signal)
         {
-            //OpenPositions(signal);
-            using (var httpClient = new HttpClient())
-            {
-                string jsonString = JsonSerializer.Serialize(signal);
-                var res = httpClient.GetAsync($"https://api.telegram.org/{token}/sendMessage?chat_id={channel}&text={jsonString}").Result;
-            }
+            OpenPositions(signal);
+            // using (var httpClient = new HttpClient())
+            // {
+            //     string jsonString = JsonSerializer.Serialize(signal);
+            //     var res = httpClient.GetAsync($"https://api.telegram.org/{token}/sendMessage?chat_id={channel}&text={jsonString}").Result;
+            // }
         }
 
         public async void OpenPositions(Position signal)
         {
+            decimal USDT = 0;
+            decimal SymbolAmount = 0;
+
             var client = new BinanceClient();
+            var data = await client.General.GetAccountInfoAsync();
+            if(signal.PositionSide == 0)
+            {
+                foreach (var item in data.Data.Balances)
+                {
+                    if(item.Asset == "USDT")
+                        USDT = item.Free;
+                }
+            }
+            else
+            {
+                string[] collection = signal.Symbol.Split('U');
+                foreach (var item in data.Data.Balances)
+                {
+                    if(item.Asset == collection[0])
+                        SymbolAmount = item.Free;
+                }
+            }
+            
 
             //decimal price = BinanceHelpers.FloorPrice(token.TickSize, position.EntryPrice);
             //decimal quantity = (position.Quantity < 0) ? Math.Round(Math.Abs(position.Quantity), Convert.ToInt32(token.QuoteAssetPrecision)) : position.Quantity;
-            // var res = await client.Spot.Order.PlaceOrderAsync(
-            //     signal.Symbol, 
-            //     OrderSide side, 
-            //     OrderType type, decimal? quantity = null, decimal? quoteOrderQuantity = null, string? newClientOrderId = null, decimal? price = null, TimeInForce? timeInForce = null, decimal? stopPrice = null, decimal? icebergQty = null, OrderResponseType? orderResponseType = null, int? receiveWindow = null, CancellationToken ct = default
+            var res = await client.Spot.Order.PlaceOrderAsync(
+                signal.Symbol, 
+                signal.PositionSide == 0 ? OrderSide.Buy : OrderSide.Sell,
+                OrderType.Market, 
+                SymbolAmount == 0 ? null : SymbolAmount, // SymbolAmount
+                USDT == 0 ? null : USDT, // quoteSymbolAmount
+                null, 
+                null, 
+                null, 
+                null, //decimal stopPrice
+                null, 
+                null, 
+                null, 
+                default
                 
-            // );
+            );
 
             // if(!res.Success)
             //     System.Console.WriteLine(res.Error.Message);
