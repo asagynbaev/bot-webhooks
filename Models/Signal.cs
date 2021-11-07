dea25a3bf744006853bca5b064f016d78e24b117
Azimbek Sagynbaev <sagynbaev6@gmail.com>
2021-11-07 04:09:45 +0100

    Abstract class has been added

--- Models/Signal.cs
+++ Models/Signal.cs
@@ -2,6 +2,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using bot_webhooks.Helpers;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
@@ -24,14 +25,10 @@ namespace bot_webhooks.Models
         
         [Column("ID")]
         [Key]
-        public int ID { get; protected set; }
-        public string Symbol { get; private set; }
-        public int BuySignalLevel1 { get; private set; }
-        public int BuySignalLevel2 { get; private set; }
-        public int BuySignalLevel3 { get; private set; }
-        public int SellSignalLevel1 { get; private set; }
-        public int SellSignalLevel2 { get; private set; }
-        public int SellSignalLevel3 { get; private set; }
+        public int ID { get; set; }
+        public string Symbol { get; set; }
+        public decimal Price { get; set; }
+        public int Direction { get; set; }
 
         public async Task<Signal> GetDataFromDBAsync(string symbol)
         {
@@ -53,12 +50,12 @@ namespace bot_webhooks.Models
             {
                 Signal statement = await Db.Signals.Where(x => x.Symbol == symbol).FirstOrDefaultAsync();
                 // FIXME: Use only one variable to measure level of signal
-                statement.BuySignalLevel1 = 0;
-                statement.BuySignalLevel2 = 0;
-                statement.BuySignalLevel3 = 0;
-                statement.SellSignalLevel1 = 0;
-                statement.SellSignalLevel2 = 0;
-                statement.SellSignalLevel3 = 0;
+                // statement.BuySignalLevel1 = 0;
+                // statement.BuySignalLevel2 = 0;
+                // statement.BuySignalLevel3 = 0;
+                // statement.SellSignalLevel1 = 0;
+                // statement.SellSignalLevel2 = 0;
+                // statement.SellSignalLevel3 = 0;
 
                 await Db.SaveChangesAsync();
                 return true;
@@ -78,27 +75,27 @@ namespace bot_webhooks.Models
             {
                 Signal statement = await Db.Signals.Where(x => x.Symbol == symbol).FirstOrDefaultAsync();
 
-                switch (signal)
-                {
-                    case 1:
-                        statement.BuySignalLevel1 = 1;
-                        break;
-                    case 2:
-                        statement.BuySignalLevel2 = 1;
-                        break;
-                    case 3:
-                        statement.BuySignalLevel3 = 1;
-                        break;
-                    case 4:
-                        statement.SellSignalLevel1 = 1;
-                        break;
-                    case 5:
-                        statement.SellSignalLevel2 = 1;
-                        break;
-                    default:
-                        statement.SellSignalLevel3 = 1;
-                        break;
-                }
+                // switch (signal)
+                // {
+                //     case 1:
+                //         statement.BuySignalLevel1 = 1;
+                //         break;
+                //     case 2:
+                //         statement.BuySignalLevel2 = 1;
+                //         break;
+                //     case 3:
+                //         statement.BuySignalLevel3 = 1;
+                //         break;
+                //     case 4:
+                //         statement.SellSignalLevel1 = 1;
+                //         break;
+                //     case 5:
+                //         statement.SellSignalLevel2 = 1;
+                //         break;
+                //     default:
+                //         statement.SellSignalLevel3 = 1;
+                //         break;
+                // }
 
                 await Db.SaveChangesAsync();
                 return true;
